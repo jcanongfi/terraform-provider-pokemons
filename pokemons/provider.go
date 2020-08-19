c038ae57bfbc051a0493e1997cf958403018ff9a
Julien CANON <julien.canon@gfi.fr>
2020-08-19 09:52:28 +0000

    Add the possibility to configure external url

--- pokemons/provider.go
+++ pokemons/provider.go
@@ -12,6 +12,11 @@ import (
 func Provider() *schema.Provider {
   return &schema.Provider{
     Schema: map[string]*schema.Schema{
+      "url": &schema.Schema{
+        Type:        schema.TypeString,
+        Optional:    true,
+        DefaultFunc: schema.EnvDefaultFunc("POKE_URL", nil),
+      },
       "username": &schema.Schema{
         Type:        schema.TypeString,
         Optional:    true,
@@ -35,13 +40,32 @@ func Provider() *schema.Provider {
 }
  
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+  url := d.Get("url").(string)
   username := d.Get("username").(string)
   password := d.Get("password").(string)
 
   var diags diag.Diagnostics
 
+  if (url != "") && (username != "") && (password != "") {
+    c, err := pokemon.NewClient(&url, &username, &password)
+    if err != nil {
+      return nil, diag.FromErr(err)
+    }
+
+    return c, diags
+  }
+
   if (username != "") && (password != "") {
     c, err := pokemon.NewClient(nil, &username, &password)
+    if err != nil {
+      return nil, diag.FromErr(err)
+    }
+
+    return c, diags
+  }
+
+  if (url != "") {
+    c, err := pokemon.NewClient(&url, nil, nil)
     if err != nil {
       return nil, diag.FromErr(err)
     }
